35cda5e25730cc527edc03f5159cfcbcd71515d8
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-01-24 11:08:09 +0700

    use latest hmkit

--- app/src/main/java/com/highmobility/basicoauth/MainActivity.kt
+++ app/src/main/java/com/highmobility/basicoauth/MainActivity.kt
@@ -1,96 +1,101 @@
 package com.highmobility.basicoauth
 
 import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
-import com.highmobility.autoapi.HonkAndFlash
+import com.highmobility.autoapi.CommandResolver
+import com.highmobility.autoapi.DiagnosticsState
+import com.highmobility.autoapi.Failure
+import com.highmobility.autoapi.GetDiagnosticsState
 import com.highmobility.crypto.value.DeviceSerial
 import com.highmobility.hmkit.HMKit
+import com.highmobility.hmkit.HMLog
 import com.highmobility.hmkit.Telematics
 import com.highmobility.hmkit.error.DownloadAccessCertificateError
 import com.highmobility.hmkit.error.TelematicsError
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
-    val TAG = "Basic OAuth"
-    private val oauthManager = OAuthManager()
-    private lateinit var textView: TextView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         /*
-            Two things need to be set for this app to work.
+        Before using HMKit, you'll have to initialise the Manager singleton
+        with a snippet from the Platform Workspace:
 
-            Look at:
-             * OAuthManager.kt fields
-             * method downloadAccessCertificate in this class
+          1. Sign in to the workspace
+          2. Go to the LEARN section and choose Android
+          3. Follow the Getting Started instructions
+
+        By the end of the tutorial you will have a snippet for initialisation,
+        that looks something like this:
+
+          HMKit.getInstance().initialise(
+            Base64String,
+            Base64String,
+            Base64String,
+            applicationContext
+          );
+        */
+
+        // TODO: REPLACE THE INITIALISE SNIPPET HERE
+        HMKit.getInstance().initialise(
+                "",
+                "",
+                "",
+                applicationContext
+        )
+
+
+        /*
+         Before using the OAuth, it's required variables must be set:
+         - go to https://high-mobility.com/profile/oauth-client to get and paste:
+            * Auth URI
+            * Client ID
+            * redirectScheme (URL-scheme for iOS & Android)
+            * Token URI
+         - go to https://high-mobility.com/develop/
+            * choose one app and copy its Serial#/App Id (hex number)
+            * IMPORTANT: The app id must be the same that you initialise the SDK with.
+         - now fill in all the needed vars
          */
-
-        textView = findViewById(R.id.text_view)
-        progressBar = findViewById(R.id.progress_bar)
-        button = findViewById(R.id.access_token_text_button)
-
         button.setOnClickListener {
-            openOAuthUrl()
+            // TODO: SET THE OAUTH VARIABLES
+            HMKit.getInstance().oAuth.getAccessToken(
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    null,
+                    null,
+                    null
+            ) { accessToken, errorMessage ->
+                if (accessToken != null) {
+                    onAccessTokenDownloaded(accessToken)
+                }
+                else {
+                    onError(errorMessage!!)
+                }
+            }
+        }
+
+
+        // optional: if have downloaded the certificate previously, can access it from HMKit storage.
+        val serial = DeviceSerial("000000000000000000")
+        val cert = HMKit.getInstance().getCertificate(serial)
+        if (cert != null) {
+            downloadVehicleStatus(cert.gainerSerial)
+            textView.text = "Have certificate. Sending Get Diagnostics"
         }
     }
 
-    private fun openOAuthUrl() {
-        val url = oauthManager.webViewUrl()
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(browserIntent)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if (intent?.data != null) {
-            Log.d(TAG, "new intent: " + intent.data)
-            progressBar.visibility = View.VISIBLE
-            button.visibility = View.GONE
-
-            oauthManager.downloadAccessToken(intent.data, this, { accessToken ->
-                downloadAccessCertificate(accessToken)
-            }, { errorMessage ->
-                progressBar.visibility = View.GONE
-                onError("error downloading access token " + errorMessage)
-            })
-        }
-    }
-
-    private fun downloadAccessCertificate(accessToken: String) {
-        /*
-         * Before using HMKit, you'll have to initialise the Manager singleton
-         * with a snippet from the Platform Workspace:
-         *
-         *   1. Sign in to the workspace
-         *   2. Go to the LEARN section and choose Android
-         *   3. Follow the Getting Started instructions
-         *
-         * By the end of the tutorial you will have a snippet for initialisation,
-         * that looks something like this:
-         *
-         *   HMKit.getInstance().initialise(
-         *     Base64String,
-         *     Base64String,
-         *     Base64String,
-         *     getApplicationContext()
-         *   );
-         */
-
-        // PASTE THE SNIPPET HERE
-
+    private fun onAccessTokenDownloaded(accessToken: String) {
         HMKit.getInstance().downloadAccessCertificate(accessToken, object : HMKit.DownloadCallback {
             override fun onDownloaded(vehicleSerial: DeviceSerial) {
-                sendHonkFlash(vehicleSerial)
+                downloadVehicleStatus(vehicleSerial)
             }
 
             override fun onDownloadFailed(error: DownloadAccessCertificateError) {
@@ -99,25 +104,32 @@ class MainActivity : Activity() {
         })
     }
 
-    private fun sendHonkFlash(vehicleSerial: DeviceSerial) {
+    private fun downloadVehicleStatus(vehicleSerial: DeviceSerial) {
+        progressBar.visibility = View.VISIBLE
+        textView.text = "Sending Get Diagnostics"
         // send a simple command to see everything worked
-        val command = HonkAndFlash(5, 1)
-        HMKit.getInstance().telematics.sendCommand(command, vehicleSerial,
-                object : Telematics.CommandCallback {
-                    override fun onCommandResponse(p0: com.highmobility.value.Bytes?) {
-                        progressBar.visibility = View.GONE
-                        textView.text = "Successfully sent honk and flash."
-                    }
+        HMKit.getInstance().telematics.sendCommand(GetDiagnosticsState(), vehicleSerial, object :
+                Telematics.CommandCallback {
+            override fun onCommandResponse(p0: com.highmobility.value.Bytes?) {
+                progressBar.visibility = View.GONE
+                val command = CommandResolver.resolve(p0)
 
-                    override fun onCommandFailed(p0: TelematicsError?) {
-                        onError("failed to send honk and flash: " + p0?.type + " " + p0?.message)
-                    }
-                })
+                when (command) {
+                    is DiagnosticsState -> textView.text = "Got Diagnostics,\nmileage: ${command.mileage}"
+                    is Failure -> textView.text = "Get Diagnostics failure:\n\n${command.failureReason}\n${command.failureDescription}"
+                    else -> textView.text = "Unknown command response"
+                }
+            }
+
+            override fun onCommandFailed(p0: TelematicsError?) {
+                onError("failed to get VS:\n" + p0?.type + " " + p0?.message)
+            }
+        })
     }
 
     private fun onError(msg: String) {
         progressBar.visibility = View.GONE
         textView.text = msg
-        Log.e(TAG, msg)
+        HMLog.e(msg)
     }
 }
